770ea1bf49da9aa65423e8e779add5c8fc09d068
Davi-Munhoz <Dah.munhoz@gmail.com>
2024-08-29 17:08:19 -0300

    main

--- PEM_2024-2-Atividades-N1-N2/PEM-Atividades-N1/PEM-Atividade-N1-3/Atividade 3 Tabuleiro de Xadrez.c
+++ PEM_2024-2-Atividades-N1-N2/PEM-Atividades-N1/PEM-Atividade-N1-3/Atividade 3 Tabuleiro de Xadrez.c
@@ -1,36 +1,27 @@
-/*--------------------------------------------------------*
-* Disciplina: Programaçao Estruturada e Modular *
-*          Prof. Carlos Veríssimo                                    *
-*--------------------------------------------------------*
-* Objetivo do Programa: Construir um tabuleiro de xadrez     *
-* Data - 29/08/2024                                               * 
-* Autor: Davi Gabriel Ribeiro Munhoz*
-*--------------------------------------------------------*/
+
 
 #include <stdio.h>
 #include <string.h>
 #define tamanho 8
-#define celula 5 
+#define celula 5
 void inicializar(char tabuleiro[tamanho][tamanho][celula]) {
-    const char* brancas[] = {"BT1 ", "BC1 ", "BB1 ", "BD1 ", "BR1 ", "BB2 ", "BC2 ", "BT2 "};
-    const char* pretas[] = {"PT1 ", "PC1 ", "PB1 ", "PD1 ", "PR1 ", "PB2 ", "PC2 ", "PT2 "};
-    
+    const char* brancas[] = {"BT1 ", "BC2 ", "BB3 ", "BD4 ", "BR5 ", "BB6 ", "BC7 ", "BT8 "};
+    const char* pretas[] = {"PT1 ", "PC2 ", "PB3 ", "PR4 ", "PD5 ", "PB6 ", "PC7 ", "PT8 "};
     for (int i = 0; i < 8; i++) {
         snprintf(tabuleiro[0][i], celula, "%s", brancas[i]);
         snprintf(tabuleiro[1][i], celula, "BP%d ", i + 1);
         snprintf(tabuleiro[6][i], celula, "PP%d ", i + 1);
         snprintf(tabuleiro[7][i], celula, "%s", pretas[i]);
     }
-    
     for (int i = 2; i < 6; i++) {
         for (int j = 0; j < 8; j++) {
             snprintf(tabuleiro[i][j], celula, "X   ");
         }
     }
 }
-
 void imprimir(char tabuleiro[tamanho][tamanho][celula]) {
     printf("  a    b   c   d   e   f   g   h\n");
+    printf("---------------------------------\n");
     for (int i = 0; i < tamanho; i++) {
         printf("%d ", tamanho - i);
         for (int j = 0; j < tamanho; j++) {
@@ -39,12 +30,9 @@ void imprimir(char tabuleiro[tamanho][tamanho][celula]) {
         printf("\n");
     }
 }
-
 int main() {
     char tabuleiro[tamanho][tamanho][celula];
-
     inicializar(tabuleiro);
     imprimir(tabuleiro);
-
     return 0;
 }
